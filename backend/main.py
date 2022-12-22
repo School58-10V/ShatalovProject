87c99e551c22c9f2ce0f52c30641322c1d487675
Иван Шаталов <isatalov70@gmail.com>
2022-12-22 16:58:31 +0300

    backend

---
+++ backend/main.py
@@ -0,0 +1,41 @@
+import json
+
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+
+@app.route("/quotes", methods=["GET"])
+def get_blog_posts():
+    with open("base.json", encoding="utf-8-sig") as f:
+        text_data = f.read()
+        data = json.loads(text_data)
+        return jsonify(data)
+
+
+@app.route("/quotes/<int:id>", methods=["GET"])
+def get_quotes_by_id(id):
+    with open("base.json", encoding="utf-8-sig") as f:
+        text_data = f.read()
+        data = json.loads(text_data)
+        quote = data["quotes"][str(id)]
+        return jsonify(quote)
+
+
+
+@app.route("/quotes", methods=["POST"])
+def create_quotes():
+    quote = request.get_json()["quote"]
+    with open("base.json", encoding="utf-8") as f:
+        data = json.load(f)
+
+    current_id = str(len(data["quotes"].keys()) + 1)
+    data["quotes"][current_id] = quote
+    with open("base.json", 'w', encoding='utf-8') as f:
+        json.dump(data, f)
+
+    return current_id
+
+
+if __name__ == "__main__":
+    app.run()
