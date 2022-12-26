5e23e4beac5d7a87a7be6114e63348184abe45dd
Иван Шаталов <isatalov70@gmail.com>
2022-12-26 15:45:25 +0300

    фронт

---
+++ frontend/src/views/CreateQuoteView.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <label for="quoteText">Текст цитаты</label>
+        <input id="quoteText">
+        <b-button @click="createQuote()" variant="danger">Создать</b-button>
+    </div>
+
+</template>
+
+<script>
+export default {
+    name: 'CreateQuoteView',
+    data() {
+        return {
+            quote: {
+                id: null,
+                title: '',
+            },
+        };
+    },
+    methods: {
+        createQuote() {
+            this.quote = document.getElementById('quoteText').value;
+            fetch('http://localhost:5000/quotes', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({quote: this.quote}),
+            })
+            .then(() => {
+                this.$router.push({name: 'home'});
+            })
+        },
+    },
+}
+</script>
